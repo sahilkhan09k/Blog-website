4afa41e82e4d17fd81db4a35df185a200a6a3d26
sahilkhan09k <sahilkhan392005@gmail.com>
2025-03-12 13:09:51 +0530

    Add authentication and user management components: Login, Signup, and Protected layout

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Button({
     children,
     type = 'button',
-    bgcolor = 'bg-blue-500',
-    textcolor = 'text-white',
+    bgColor = 'bg-blue-500',
+    textColor = 'text-white',
     className = '',
     ...props
  }) {
   return (
-   <button className={`px-4 py-2 rounded-lg ${bgcolor} ${textcolor} ${className}`} type={type} {...props}>
+   <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} type={type} {...props}>
     {children}
    </button>
   )
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import Button from "./Button.jsx";
+import Input from "./Input.jsx";
 
 export {
-     Header, 
-     Footer,
      Container,
      Logo,
-     LogoutBtn
+     LogoutBtn,
+     Button,
+     Input
 };
